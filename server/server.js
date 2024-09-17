9782a483eee5753905f3fa93746bef8d46750ae4
nguyenduccanh1523 <nguyenduccanh2398@gmail.com>
2024-09-17 13:16:55 +0700

    create Models Project

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 require('dotenv').config()
 import cors from 'cors'
 import initRoutes from './src/routes'
-
+import connectDatabase from './src/config/connectDatabase'
 
 const app = express()
 app.use(cors({
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 initRoutes(app)
-
+connectDatabase()
 
 const port = process.env.PORT || 8888
 const listener = app.listen(port, () => {
